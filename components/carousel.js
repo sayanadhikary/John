edc268fa87d6848d890064a87bd48854b47ba67a
sayan <sayan711226@gmail.com>
2024-07-25 19:48:23 +0530

    fixed carousel by adding height class

--- components/carousel.js
+++ components/carousel.js
@@ -9,13 +9,13 @@ export default function Slideshow(){
     <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
   <div className="carousel-inner">
     <div className="carousel-item active" data-bs-interval="5000">
-      <Image src={carImage3} width={0} height={0} className="d-block w-100" alt="slideOne" />
+      <Image src={carImage1} width={0} height={0} className="d-block w-100 h-100" alt="slideOne" />
     </div>
     <div className="carousel-item" data-bs-interval="5000">
-      <Image src={carImage1} width={0} height={0} className="d-block w-100" alt="slideTwo"/>
+      <Image src={carImage3} width={0} height={0} className="d-block w-100 h-100" alt="slideTwo"/>
     </div>
     <div className="carousel-item" data-bs-interval="5000">
-      <Image src={carImage5} width={0} height={0} className="d-block w-100" alt="slideThree" />
+      <Image src={carImage5} width={0} height={0} className="d-block w-100 h-100" alt="slideThree" />
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
